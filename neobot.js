b9c52006305d53cba2cc6efeff465eca670e2a34
octobr <m4ttbruno@gmail.com>
2021-05-17 12:23:59 -0400

    'pwease'

--- neobot.js
+++ neobot.js
@@ -42,17 +42,41 @@ bot.on('/userscripts', function (msg) {
 	    return bot.sendMessage(msg.chat.id, text, {parseMode, webPreview});
 });
 //neopet image get
+// On inline query
 bot.on('inlineQuery', msg => {
-	let query = msg.query;
-	console.log('inline query: ${ query }');
-	petImage('${ query }').then(data => console.log(data)).catch(console.error);
-	answers.addPhoto({
-		id: 'photo',
-		caption: '${ query } looks real cute',
-		photo_url: 'http://pets.neopets.com/cp/qn8kjbwf/1/5.png',
-		thumb_url: 'http://pets.neopets.com/cp/qn8kjbwf/1/5.png'
-	});
-	return bot.answerQuery(answers);
+
+    let query = msg.query;
+    console.log(`inline query: ${ query }`);
+
+    // Create a new answer list object
+    const answers = bot.answerList(msg.id, {cacheTime: 60});
+
+    // Article
+    answers.addArticle({
+        id: 'query',
+        title: 'Inline Title',
+        description: `Your query: ${ query }`,
+        message_text: 'Click!'
+    });
+
+    // Photo
+    answers.addPhoto({
+        id: 'photo',
+        caption: 'Telegram logo.',
+        photo_url: 'https://telegram.org/img/t_logo.png',
+        thumb_url: 'https://telegram.org/img/t_logo.png'
+    });
+
+    // Gif
+    answers.addGif({
+        id: 'gif',
+        gif_url: 'https://telegram.org/img/tl_card_wecandoit.gif',
+        thumb_url: 'https://telegram.org/img/tl_card_wecandoit.gif'
+    });
+
+    // Send answers
+    return bot.answerQuery(answers);
+
 });
 //games list
 var lastMessage;
