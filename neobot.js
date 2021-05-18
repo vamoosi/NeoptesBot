a621f01187c14c459bfe425d1e329c0e87900a10
octobr <m4ttbruno@gmail.com>
2021-05-18 19:37:23 -0400

    'pls'

--- neobot.js
+++ neobot.js
@@ -42,38 +42,10 @@ bot.on('/userscripts', function (msg) {
 	    return bot.sendMessage(msg.chat.id, text, {parseMode, webPreview});
 });
 //neopet image get
-var lastMessage;
-var photoUrl = 'https://telegram.org/img/tl_card_destruct.gif';
 
-bot.on('/pic', msg => {
-
-    // Send image with caption
-    return bot.sendPhoto(
-        msg.from.id, photoUrl, {caption: 'This is a default caption.'}
-    ).then(re => {
-        // Get message id and chat
-        lastMessage = [msg.from.id, re.result.message_id];
-        bot.sendMessage(msg.from.id, 'Now set a new caption using /edit <caption>');
-    });
-
-});
-
-bot.on('/edit', msg => {
-
-    if (!lastMessage) {
-        return bot.sendMessage(msg.from.id, 'Type /start and then /edit <caption>');
-    }
-
-    let [chatId, messageId] = lastMessage;
-    let caption = msg.text.replace('/edit ', '');
-
-    if (caption == '/edit') caption = 'No caption.';
-
-    // Change caption
-    return bot.editMessageCaption({chatId, messageId}, caption).then(() => {
-        bot.sendMessage(msg.from.id, `Caption changed to: ${ caption }`);
-    });
-
+bot.on(/^\/say (.+)$/, (msg, props) => {
+	const text = props.match[1];
+    return bot.sendMessage(msg.from.id, text, { replyToMessage: msg.message_id });
 });
 //games list
 var lastMessage;
